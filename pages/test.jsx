9134f6c8cc0bc67dcb7610be9f15038d43033dc4
Edgarsc0 <esanchezc2004@alumno.ipn.mx>
2023-05-01 19:07:29 -0600

    nueva act

--- pages/test.jsx
+++ pages/test.jsx
@@ -1,22 +1,30 @@
 import { useEffect, useState } from "react"
 
 const Test = () => {
+    const [id, setId] = useState();
     const [coords, setCoords] = useState([]);
-
+    const [errors, setErrors] = useState([]);
     useEffect(() => {
-        const success = (position) => {
-            console.log(position);
+        const sucess = (position) => {
             setCoords([...coords, [position.coords.latitude, position.coords.longitude]]);
         }
-        navigator.geolocation.getCurrentPosition(success);
+        const error = (err) => {
+            setErrors([...errors,[err]]);
+        }
+        const id = navigator.geolocation.watchPosition(sucess, error, { enableHighAccuracy: true, timeout: 5000 });
+        setId(id);
     }, [])
     return (
         <>
-            {
-                coords.map(cords=>(
-                    <><p>{JSON.stringify(cords)}</p><br></br></>
-                ))
-            }
+            <div>{errors.map(item => (
+                <><p>{JSON.stringify(item)}</p><br></br></>
+            ))}</div>
+            <h1>Longitud del estado: {coords.length}</h1>
+            <hr></hr>
+            <br></br>
+            {coords.map(item => (
+                <><p>{JSON.stringify(item)}</p><br></br></>
+            ))}
         </>
     );
 }
